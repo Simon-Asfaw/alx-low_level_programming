272fea44d662792fff7b40c22d8a416590271cc9
Simon-Asfaw <simonasfaw587@gmail.com>
2023-02-20 17:42:32 +0000

    what 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - boss
+ * Description: does stuff
+ * Return: zero Nada
+ */
+int main(void)
+{
+	char c;
+	int i;
+	float f;
+	long int l;
+	long long int ll;
+
+	printf("Size of a char:", sizeof(c));
+	printf("Size of an int:", sizeof(i));
+	printf("Size of a long int:", sizeof(l));
+	printf("Size of a long long int:", sizeof(ll));
+	printf("Size of a float:", sizeof(f));
+	return (0);
+}
+
+
+
