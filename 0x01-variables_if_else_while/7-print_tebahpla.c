ed22375762e8fe3a84ca07b5cb6d994f5f4704b6
Simon-Asfaw <simonasfaw587@gmail.com>
2023-02-20 20:49:49 +0000

    7 ans

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - wa
+ * Description: na
+ * Return: di
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a <= 'a' ; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
