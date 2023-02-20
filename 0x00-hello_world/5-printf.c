62fad43de4015c3fc984aad7a655b535b2df6078
Simon-Asfaw <simonasfaw587@gmail.com>
2023-02-20 17:29:16 +0000

    ans 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the function that starts
+ * Description: what we gonna do is use printf
+ * Return: zero 0 nada
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
