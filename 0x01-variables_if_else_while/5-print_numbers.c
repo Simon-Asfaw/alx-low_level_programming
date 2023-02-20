a7c02b99b502be4bd813335c38c0e46a0609158b
Simon-Asfaw <simonasfaw587@gmail.com>
2023-02-20 20:36:01 +0000

    5 ans

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - wa
+ * Description: na
+ * Return: di
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10 ; a++)
+	{
+		printf("%d\n",a);
+	}
+	return (0);
+}
+
