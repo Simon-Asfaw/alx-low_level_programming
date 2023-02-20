32c64272520e961f68f47793459094eacee14ad8
Simon-Asfaw <simonasfaw587@gmail.com>
2023-02-20 20:20:56 +0000

    3 ans

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - wa
+ * Description: na
+ * Return: di
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
