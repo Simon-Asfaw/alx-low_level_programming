93237bc0afe675dcbd54443f9325a5daa07e996f
Simon-Asfaw <simonasfaw587@gmail.com>
2023-02-20 18:52:07 +0000

    bon 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <iostream.h>
+/**
+ * main - duh
+ * Description: w
+ * Return: my dog
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
