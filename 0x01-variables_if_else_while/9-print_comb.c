9418377ea6a1829415041d59f913bb89bc3a38d5
Simon-Asfaw <simonasfaw587@gmail.com>
2023-02-20 21:03:42 +0000

    9 ans

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - wa
+ * Description: na
+ * Return: di
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9 ; a++)
+	{
+		putchar(a + '0')
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
