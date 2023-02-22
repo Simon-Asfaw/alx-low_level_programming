41996e24eefaeecd35c3479b6f9161635793d75b
Simon-Asfaw <simonasfaw587@gmail.com>
2023-02-22 16:47:16 +0000

    waq 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks low
+ * Description: wew
+ * Return: 1 0r 0
+ * @c : we check
+ */
+
+int _islower(int c)
+{
+	if ('a' <= c <= 'z')
+		return (1);
+	else
+		return (0);
+}
