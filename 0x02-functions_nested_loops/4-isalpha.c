31c9ce08a195fe8f0c704e95ebdc8dd144d56c0d
Simon-Asfaw <simonasfaw587@gmail.com>
2023-02-22 17:25:18 +0000

    submit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,13 @@
 
 /**
  * _isalpha - checks for alphabet
- * @c : is the var
- * Return : my mango 1 0r0
+ * @c: is the var
+ * Return: my mango 1 0r0
  */
 
 int _isalpha(int c)
 {
+	/* _isalpha does stuff */
 	if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
 		return (1);
 	else
