ebd3dd51bd48026468a6452b8e9f394cf2ec1c05
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-01 08:16:46 +0000

    ptr ans 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * swap_int - swap num
+ * @a: var1
+ * @b: var2
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *p;
+	int *p2;
+
+	*p = a;
+	*p2 = b;
+	*a = p2;
+	*b = p;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,9 +4,13 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  * @*n: the pointer thing
+ * @*a: val1
+ * @*b: val2
  * reset_to-98 - ch val ptr
+ * swap_int - swap val
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
