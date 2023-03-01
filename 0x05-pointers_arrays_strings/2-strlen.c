87ebce90fb5269f8ba1a0277708bb746e10958f6
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-01 10:48:09 +0000

    ptr ans 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 
 /**
- * _strlen - the length of 
+ * _strlen - the length of
  * @s: input string
  * Return: int
  */
 
 int _strlen(char *s)
 {
-	return(sizeof(&s));
+	int temp = 0;
+
+	while (s != '\0')
+	{
+		s++;
+		temp++;
+	}
+	return (temp);
 }
