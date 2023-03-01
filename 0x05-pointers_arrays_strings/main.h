a47cfdba05b75a4b405fc5755fb7a1b5a7f576b5
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-01 12:30:43 +0000

    ptr ans 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,9 +6,11 @@
  * @*n: the pointer thing
  * @*a: val1
  * @*b: val2
+ * @str: string
  * reset_to-98 - ch val ptr
  * swap_int - swap val
  * _strlen - len of str
+ * _puts - print stuff
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -16,3 +18,4 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
