0fe6b5f76f69ea4d4028d13e510be5071bee8741
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-03 11:16:00 +0000

    ans 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,25 +9,23 @@
 void jack_bauer(void)
 {
 	int H;
-	int h;
 	int M;
 	int m;
 
-	for (H = 0; H < 3; H++)
+	H = 24;
+
+	while (H < 24)
 	{
-		for (h = 0; h < 4; h++)
+		for (M = 0; M < 6; M++)
 		{
-			for (M = 0; M < 6; M++)
+			for (m = 0; m < 10; m++)
 			{
-				for (m = 0; m < 10; m++)
-				{
-					_putchar('0' + H);
-					_putchar('0' + h);
-					_putchar(':');
-					_putchar('0' + M);
-					_putchar('0' + m);
-					_putchar('\n');
-				}
+				_putchar('0' + H / 10);
+				_putchar('0' + H % 10);
+				_putchar(':');
+				_putchar('0' + M);
+				_putchar('0' + m);
+				_putchar('\n');
 			}
 		}
 	}
