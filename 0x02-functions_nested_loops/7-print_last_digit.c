db75d6caca274b428a0f8be1ff797268afbbfbcc
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-03 09:30:23 +0000

    ans 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last dig
+ *
+ * Return: int
+ */
+
+int print_last_digit(int)
+{
+	int n;
+	int a;
+
+	if (n % 10 >= 0)
+	{
+		a = n % 10;
+		return (a);
+	}
+	else
+	{
+		a = n % 10;
+		a = _abs(a);
+		return (a);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,10 +8,12 @@
  * print_alphabet - alpha
  * print_alphabet_x10 - 10x
  * _abs: abbs
+ * print_last_digit - prints last dig
  * print_sign - sing pring
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -19,3 +21,4 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int);
