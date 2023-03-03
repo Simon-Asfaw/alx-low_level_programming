6453058141c91b3a0280386e4d7ddec13af09302
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-03 13:43:48 +0000

    ans 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add 2 nums
+ *
+ * @i: first num
+ *
+ * @j: second num
+ *
+ * Return: int
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,6 +13,7 @@
  * print_sign - sing pring
  * times_table - tt
  * jack_bauer - jack
+ * add - add 1 and 2
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -27,3 +28,4 @@ int _abs(int);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
