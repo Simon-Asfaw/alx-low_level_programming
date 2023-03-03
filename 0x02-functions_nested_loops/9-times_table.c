cd11c2727db9cd38342636d198e07f01296aa3a2
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-03 13:26:17 +0000

    ans 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,16 +11,25 @@ void times_table(void)
 	int j;
 	int i;
 	int z;
+	char num1;
+	char num2;
+	char num3;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			z = i * j;
-			_putchar('0' + z);
+			if (z < 10)
+			{
+				nim1 = ' ';
+				num2 = ' ';
+				num3 = z;
+			}
+			_putchar('0' + num1);
+			_putchar('0' + num2);
+			_putchar('0' + num3);
 			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
