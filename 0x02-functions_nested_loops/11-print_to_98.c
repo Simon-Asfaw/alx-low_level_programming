6563db6eec97b2fe74f325bd1aab7b586ff91b6d
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-04 05:07:47 +0000

    ans 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,21 +13,31 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n < 98)
+		while (n < 99)
 		{
-			printf("%d, ",n);
+			if (n == 98)
+			{
+				printf("%d", n);
+				break;
+			}
+			printf("%d, ", n);
 			n++;
 		}
 	}
 	else if (n > 98)
 	{
-		while (n > 98)
+		while (n > 99)
 		{
-			printf("%d, ",n);
+			if (n == 98)
+			{
+				printf("%d", n);
+				break;
+			}
+			printf("%d, ", n);
 		}
 	}
 	else
 	{
-		printf("%d, ",n);
+		printf("%d, ", n);
 	}
 }
