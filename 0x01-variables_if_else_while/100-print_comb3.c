dcbeea9331f96deb20c1ab9674738520ddded6bd
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-04 19:22:04 +0000

    ans 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,18 @@ int main(void)
 	int a;
 	int b;
 
+	b = 1;
+
 	for (a = 0; a <= 8 ; a++)
 	{
-		for (b = 1; b <= 9; b++)
+		for (; b <= 9; b++)
 		{
 			putchar(a);
 			putchar(b);
 			putchar(',');
 			putchar(' ');
 		}
+		b++;
 	}
 	putchar('\n');
 	return (0);
