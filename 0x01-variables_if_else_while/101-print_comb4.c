8be7645adec21c896d1e2b4b0b892178d0b41f7c
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-04 19:50:42 +0000

    ans 10

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - prints unique combo of numbers
+ * Description: prints numbers
+ * Return: void
+ */
+
+int main(void)
+{
+	int j;
+	int a;
+	int b;
+	int z;
+	int i;
+
+	z = 1;
+	i = 2;
+
+	for (j = i; j <= 7; j++)
+	{
+		for (a = z; a <= 8 ; a++)
+		{
+			for (b = 2; b <= 9; b++)
+			{
+				putchar('0' + a);
+				putchar('0' + b);
+				if (j == 7 && a == 8 && b == 9)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			z++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
