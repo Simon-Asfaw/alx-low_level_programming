df4b3a085b47fd0d0c00caf4752406a57c354ddf
Simon-Asfaw <simonasfaw587@gmail.com>
2023-03-04 19:35:43 +0000

    ans 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,8 +20,11 @@ int main(void)
 		{
 			putchar('0' + a);
 			putchar('0' + b);
-			putchar(',');
-			putchar(' ');
+			if (a != 8 && b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 		z++;
 	}
